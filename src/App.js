047665f38bd341020f631630704e8e5c61f93636
Ayodele Ayodeji <awesomedursky@gmail.com>
2023-05-09 16:33:03 +0100

    complete commit of a simple monster cards app... monster pictures consumed from https://jsonplaceholder.typicode.com/users

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-field'
+
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    } 
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(i => this.setState({ monsters: i }))
+  }
+
+  render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return (
+      <div className="App">
+        <h1 className='h'>Monster Rholodex</h1>
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={
+            e => {
+              this.setState(
+                { searchField: e.target.value }
+              )
+            }
+          }
+        />
+        
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
